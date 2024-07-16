0150385c28d6a7e4f979d3da7b38ce62a9e6cfcf
Nikita Nagpal <nikkinagpal22@gmail.com>
2024-07-16 15:03:40 +0530

    initial commit

--- src/components/DashBoard/DashBoard.jsx
+++ src/components/DashBoard/DashBoard.jsx
@@ -7,28 +7,61 @@ import DateTime from "../DateTime/DateTime";
 import "./DashBoard.css";
 import SearchBar from "../searchbar/Searchbar";
 const DashBoard = () => {
-    return (
-        <div>
-            <SearchBar />
-            <div className="dashboard">
-                <div className="maincol">
-                    <Location />
-                    <Banner max={30} min={20} temperature={25} status={"Amazing weather"} />
-                    <div className="stats">
-                        <InfoCard field="humidity" value="100%" />
-                        <InfoCard field="wind" value="100%" />
-                        <InfoCard field="rain" value="100%" />
-                        <InfoCard field="feels like" value="100%" />
-                    </div>
-                </div>
-                <div className="sidecol">
-                    <DateTime />
-                    <TimeCard time={"10:29"} amORpm={"AM"} />
-                    <TimeCard time={"10:29"} amORpm={"AM"} />
-                </div>
-            </div>
+  return (
+    <div>
+      <SearchBar />
+      <div className='dashboard'>
+        <div className='locDate'>
+          <Location />
+          <div className='date'>
+            <DateTime />
+          </div>
         </div>
-    );
+        <div className='mainside'>
+          <div className='maincol'>
+            <Banner
+              max={30}
+              min={20}
+              temperature={25}
+              status={"Amazing weather"}
+            />
+            <div className='stats'>
+              <InfoCard
+                field='humidity'
+                value='100%'
+              />
+              <InfoCard
+                field='wind'
+                value='100%'
+              />
+              <InfoCard
+                field='rain'
+                value='100%'
+              />
+              <InfoCard
+                field='feels like'
+                value='100%'
+              />
+            </div>
+          </div>
+          <div className='sidecol'>
+            <div className='s1'>
+              <TimeCard
+                time={"10:29"}
+                amORpm={"AM"}
+              />
+            </div>
+            <div className='s2'>
+              <TimeCard
+                time={"10:29"}
+                amORpm={"AM"}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashBoard;
--- src/components/DateTime/DateTime.jsx
+++ src/components/DateTime/DateTime.jsx
@@ -42,7 +42,7 @@ const DateTime = () => {
         <div className="datetime">
             <div className="time">
                 <span>{timeString}</span>
-                <span style={{'font-size': "0.9rem", marginLeft : "0.5ch"}}>{ampm}</span>
+                <span style={{'font-size': "1rem", marginLeft : "0.5ch"}}>{ampm}</span>
             </div>
             <div className="dateText">{formattedDate}</div>
         </div>
